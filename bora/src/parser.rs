da84ab761b155831270a063fc3c686940b12de7a
Rogério Araújo <rogerio.araujo@gmail.com>
2025-08-25 20:38:11 -0300

    feat: added support for post, delete, put and patch on bora, README updates

--- bora/src/parser.rs
+++ bora/src/parser.rs
@@ -29,6 +29,10 @@ impl Parse for BoraApi {
 #[allow(non_camel_case_types)]
 pub enum OperationEnum {
     get(GetStruct),
+    post(PostStruct),
+    put(PutStruct),
+    delete(DeleteStruct),
+    patch(PatchStruct),
 }
 
 const METHODS: [&str; 9] = [
@@ -42,6 +46,10 @@ impl Parse for OperationEnum {
             let ident = input.parse::<Ident>()?;
             match ident.to_string().as_str() {
                 "get" => Ok(OperationEnum::get(GetStruct::parse(input)?)),
+                "post" => Ok(OperationEnum::post(PostStruct::parse(input)?)),
+                "put" => Ok(OperationEnum::put(PutStruct::parse(input)?)),
+                "delete" => Ok(OperationEnum::delete(DeleteStruct::parse(input)?)),
+                "patch" => Ok(OperationEnum::patch(PatchStruct::parse(input)?)),
                 _ => Err(input.error(format!("expected one of {METHODS:?}, found '{ident}'"))),
             }
         } else {
@@ -90,6 +98,164 @@ impl Parse for GetFieldEnum {
     }
 }
 
+pub struct PostStruct {
+    pub fields: Punctuated<PostFieldEnum, Token![,]>,
+}
+
+impl Parse for PostStruct {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let content;
+        parenthesized!(content in input);
+        Ok(PostStruct {
+            fields: content.parse_terminated(PostFieldEnum::parse, Token![,])?,
+        })
+    }
+}
+
+#[allow(non_camel_case_types)]
+pub enum PostFieldEnum {
+    name(NameStruct),
+    path(PathStruct),
+    req_body(Box<ReqBodyStruct>),
+}
+
+impl Parse for PostFieldEnum {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(Ident) {
+            let ident = input.parse::<Ident>()?;
+            match ident.to_string().as_str() {
+                "name" => Ok(PostFieldEnum::name(NameStruct::parse(input)?)),
+                "path" => Ok(PostFieldEnum::path(PathStruct::parse(input)?)),
+                "req_body" => Ok(PostFieldEnum::req_body(Box::new(ReqBodyStruct::parse(input)?))),
+                _ => Err(input.error(format!(
+                    "expected one of name, path or req_body, found '{ident}'"
+                ))),
+            }
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+pub struct PutStruct {
+    pub fields: Punctuated<PutFieldEnum, Token![,]>,
+}
+
+impl Parse for PutStruct {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let content;
+        parenthesized!(content in input);
+        Ok(PutStruct {
+            fields: content.parse_terminated(PutFieldEnum::parse, Token![,])?,
+        })
+    }
+}
+
+#[allow(non_camel_case_types)]
+pub enum PutFieldEnum {
+    name(NameStruct),
+    path(PathStruct),
+    req_body(Box<ReqBodyStruct>),
+}
+
+impl Parse for PutFieldEnum {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(Ident) {
+            let ident = input.parse::<Ident>()?;
+            match ident.to_string().as_str() {
+                "name" => Ok(PutFieldEnum::name(NameStruct::parse(input)?)),
+                "path" => Ok(PutFieldEnum::path(PathStruct::parse(input)?)),
+                "req_body" => Ok(PutFieldEnum::req_body(Box::new(ReqBodyStruct::parse(input)?))),
+                _ => Err(input.error(format!(
+                    "expected one of name, path or req_body, found '{ident}'"
+                ))),
+            }
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+pub struct DeleteStruct {
+    pub fields: Punctuated<DeleteFieldEnum, Token![,]>,
+}
+
+impl Parse for DeleteStruct {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let content;
+        parenthesized!(content in input);
+        Ok(DeleteStruct {
+            fields: content.parse_terminated(DeleteFieldEnum::parse, Token![,])?,
+        })
+    }
+}
+
+#[allow(non_camel_case_types)]
+pub enum DeleteFieldEnum {
+    name(NameStruct),
+    path(PathStruct),
+}
+
+impl Parse for DeleteFieldEnum {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(Ident) {
+            let ident = input.parse::<Ident>()?;
+            match ident.to_string().as_str() {
+                "name" => Ok(DeleteFieldEnum::name(NameStruct::parse(input)?)),
+                "path" => Ok(DeleteFieldEnum::path(PathStruct::parse(input)?)),
+                _ => Err(input.error(format!(
+                    "expected one of name, path or req_body, found '{ident}'"
+                ))),
+            }
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+pub struct PatchStruct {
+    pub fields: Punctuated<PatchFieldEnum, Token![,]>,
+}
+
+impl Parse for PatchStruct {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let content;
+        parenthesized!(content in input);
+        Ok(PatchStruct {
+            fields: content.parse_terminated(PatchFieldEnum::parse, Token![,])?,
+        })
+    }
+}
+
+#[allow(non_camel_case_types)]
+pub enum PatchFieldEnum {
+    name(NameStruct),
+    path(PathStruct),
+    req_body(Box<ReqBodyStruct>),
+}
+
+impl Parse for PatchFieldEnum {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(Ident) {
+            let ident = input.parse::<Ident>()?;
+            match ident.to_string().as_str() {
+                "name" => Ok(PatchFieldEnum::name(NameStruct::parse(input)?)),
+                "path" => Ok(PatchFieldEnum::path(PathStruct::parse(input)?)),
+                "req_body" => Ok(PatchFieldEnum::req_body(Box::new(ReqBodyStruct::parse(input)?))),
+                _ => Err(input.error(format!(
+                    "expected one of name, path or req_body, found '{ident}'"
+                ))),
+            }
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
 pub struct NameStruct {
     _equal_token: Token![=],
     pub value: LitStr,
@@ -112,6 +278,20 @@ pub struct PathStruct {
 impl Parse for PathStruct {
     fn parse(input: ParseStream) -> Result<Self, syn::Error> {
         Ok(PathStruct {
+            _equal_token: input.parse()?,
+            value: input.parse()?,
+        })
+    }
+}
+
+pub struct ReqBodyStruct {
+    _equal_token: Token![=],
+    pub value: Type,
+}
+
+impl Parse for ReqBodyStruct {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        Ok(ReqBodyStruct {
             _equal_token: input.parse()?,
             value: input.parse()?,
         })
--- deboa/src/lib.rs
+++ deboa/src/lib.rs
@@ -429,6 +429,47 @@ impl Deboa {
         Ok(self)
     }
 
+    #[cfg(feature = "msgpack")]
+    /// Allow set msgpack body at any time.
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - The data to be serialized, it must be a struct that implements Serialize.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use deboa::{Deboa, DeboaError};
+    /// use serde::Serialize;
+    ///
+    /// #[derive(Serialize)]
+    /// struct Post {
+    ///     id: u32,
+    ///     title: String,
+    ///     body: String,
+    /// }
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> Result<(), DeboaError> {
+    ///   let mut api = Deboa::new("https://jsonplaceholder.typicode.com");
+    ///   api.set_msgpack(Post { id: 1, title: "title".to_string(), body: "body".to_string() })?.post("/posts").await?;
+    ///   Ok(())
+    /// }
+    /// ```
+    ///
+    pub fn set_msgpack<T: Serialize>(&mut self, data: T) -> Result<&mut Self, DeboaError> {
+        let result = rmp_serde::to_vec(&data);
+        if let Err(error) = result {
+            return Err(DeboaError::SerializationError {
+                message: error.to_string(),
+            });
+        }
+
+        self.body = Some(result.unwrap());
+
+        Ok(self)
+    }
+
     /// Allow set text body at any time.
     ///
     /// # Arguments
