0cc6cb640f953bb65eec91c7ed35be2aeaf7954c
Rogério Araújo <rogerio.araujo@gmail.com>
2025-08-25 09:15:30 -0300

    [feat] adding simple example
    Former-commit-id: 8276184cbb11f91ead1357aaa26535c67b89598b
    Former-commit-id: faf820378fe66b21504dbe1b42814fcc5a3f776a

---
+++ examples/simple/src/main.rs
@@ -0,0 +1,23 @@
+use deboa::DeboaError;
+
+
+#[derive(Debug, serde::Deserialize)]
+pub struct Post {
+    pub id: u64,
+    pub title: String,
+    pub body: String,
+}
+
+#[tokio::main]
+
+async fn main() -> Result<(), DeboaError> {
+  use deboa::Deboa;
+
+  let api = Deboa::new("https://jsonplaceholder.typicode.com");
+  
+  let posts: Vec<Post> = api.get("/posts").await?.json::<Vec<Post>>().await?;
+  
+  println!("posts: {:#?}", posts);    
+
+  Ok(())  
+}
