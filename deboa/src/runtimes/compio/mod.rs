eacda587f24b3572e87aad175ae5dd6d718b678a
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2025-08-27 22:54:22 +0200

    [fix]: Solved conflicts

--- deboa/src/runtimes/compio/mod.rs
+++ deboa/src/runtimes/compio/mod.rs
@@ -4,6 +4,7 @@
 use std::str::FromStr;
 
 use anyhow::Result;
+use bytes::Bytes;
 use cyper_core::{HttpStream, TlsBackend};
 #[cfg(feature = "http1")]
 use hyper::client::conn::http1::{Connection, SendRequest};
@@ -12,7 +13,12 @@ use hyper::client::conn::http2::{Connection, SendRequest};
 use hyper::Uri;
 use url::Url;
 
-pub async fn get_connection(url: &Url) -> Result<(SendRequest<String>, Connection<HttpStream, String>)> {
+pub async fn get_connection(
+    url: &Url,
+) -> Result<(
+    SendRequest<http_body_util::Full<Bytes>>,
+    Connection<HttpStream, http_body_util::Full<Bytes>>,
+)> {
     let uri = Uri::from_str(url.as_str()).unwrap();
 
     let stream = HttpStream::connect(uri, TlsBackend::default()).await?;
