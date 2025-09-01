4bcac76c3fd0aa7e659b2d15c6df53d16508aac8
Araujo, Rogerio <ROGERIO_ARAUJO+thdgit@homedepot.com>
2025-09-01 16:32:11 -0300

    test: fixes unit tests for xml and msgpack formats.
    Former-commit-id: 3db69857ff760e9f2fb252daa1a09d299514d368
    Former-commit-id: b3ff29d2fd4323147753d37d9df4888b7bbf5578

--- deboa-extras/src/http/serde/msgpack.rs
+++ deboa-extras/src/http/serde/msgpack.rs
@@ -1,52 +1,28 @@
+use deboa::{
+    errors::DeboaError,
+    http::serde::{RequestBody, ResponseBody},
+};
+use serde::{Deserialize, Serialize};
+
 pub struct MsgPackBody;
 
 impl RequestBody for MsgPackBody {
-    /// Allow set msgpack body at any time.
-    ///
-    /// # Arguments
-    ///
-    /// * `data` - The data to be serialized, it must be a struct that implements Serialize.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use deboa::{Deboa, DeboaError};
-    /// use serde::Serialize;
-    ///
-    /// #[derive(Serialize)]
-    /// struct Post {
-    ///     id: u32,
-    ///     title: String,
-    ///     body: String,
-    /// }
-    /// /*
-    /// #[tokio::main]
-    /// async fn main() -> Result<(), DeboaError> {
-    ///   let mut api = Deboa::new("https://jsonplaceholder.typicode.com")?;
-    ///   api.set_msgpack(Post { id: 1, title: "title".to_string(), body: "body".to_string() })?.post("/posts").await?;
-    ///   Ok(())
-    /// }
-    /// */
-    /// ```
-    ///
     fn serialize<T: Serialize>(&self, data: T) -> Result<Vec<u8>, DeboaError> {
         let result = rmp_serde::to_vec(&data);
         if let Err(error) = result {
-            return Err(DeboaError::SerializationError { message: error.to_string() });
+            return Err(DeboaError::Serialization { message: error.to_string() });
         }
 
-        self.body = Some(result.unwrap());
-
-        Ok(self)
+        Ok(result.unwrap())
     }
 }
 
 impl ResponseBody for MsgPackBody {
-    fn deserialize<T: Deserialize<'_>>(&self, body: Vec<u8>) -> Result<T, DeboaError> {
+    fn deserialize<T: for<'a> Deserialize<'a>>(&self, body: Vec<u8>) -> Result<T, DeboaError> {
         let binding = body;
         let body = binding.as_ref();
 
-        let json = serde_json::from_slice(body);
+        let json = rmp_serde::from_slice(body);
 
         match json {
             Ok(deserialized_body) => Ok(deserialized_body),
--- deboa-extras/src/tests/http/serde/xml.rs
+++ deboa-extras/src/tests/http/serde/xml.rs
@@ -1,3 +1,12 @@
+use deboa::{Deboa, errors::DeboaError};
+use http::header;
+use httpmock::{Method::GET, MockServer};
+
+use crate::{
+    http::serde::xml::XmlBody,
+    tests::types::{Post, XML_POST, sample_post},
+};
+
 #[tokio::test]
 async fn test_set_xml() -> Result<(), DeboaError> {
     use crate::tests::types::{XML_POST, sample_post};
@@ -6,9 +15,9 @@ async fn test_set_xml() -> Result<(), DeboaError> {
 
     let data = sample_post();
 
-    let _ = api.set_xml(data)?;
+    let _ = api.set_body_as::<XmlBody, Post>(XmlBody, data)?;
 
-    assert_eq!(api.body, Some(XML_POST.to_vec()));
+    assert_eq!(api.raw_body(), &XML_POST.to_vec());
 
     Ok(())
 }
@@ -21,9 +30,7 @@ async fn test_xml_response() -> Result<(), DeboaError> {
 
     let http_mock = server.mock(|when, then| {
         when.method(GET).path("/posts/1");
-        then.status(200)
-            .header(header::CONTENT_TYPE.as_str(), crate::APPLICATION_XML)
-            .body(XML_POST);
+        then.status(200).header(header::CONTENT_TYPE.as_str(), "application/xml").body(XML_POST);
     });
 
     let server_address = *server.address();
@@ -32,10 +39,10 @@ async fn test_xml_response() -> Result<(), DeboaError> {
     let port = server_address.port();
 
     let mut api = Deboa::new(&format!("http://{ip}:{port}"))?;
-    api.edit_header(header::CONTENT_TYPE, crate::APPLICATION_XML.to_string());
-    api.edit_header(header::ACCEPT, crate::APPLICATION_XML.to_string());
+    api.edit_header(header::CONTENT_TYPE, "application/xml".to_string());
+    api.edit_header(header::ACCEPT, "application/xml".to_string());
 
-    let response = api.get("/posts/1").await?.xml::<Post>().await?;
+    let response = api.get("/posts/1").await?.body_as::<XmlBody, Post>(XmlBody)?;
 
     http_mock.assert();
 
