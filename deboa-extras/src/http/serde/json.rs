3db69857ff760e9f2fb252daa1a09d299514d368
Araujo, Rogerio <ROGERIO_ARAUJO+thdgit@homedepot.com>
2025-09-01 16:32:11 -0300

    test: fixes unit tests for xml and msgpack formats.

--- deboa-extras/src/http/serde/json.rs
+++ deboa-extras/src/http/serde/json.rs
@@ -7,34 +7,6 @@ use serde::{Deserialize, Serialize};
 pub struct JsonBody;
 
 impl RequestBody for JsonBody {
-    /// Allow set json body at any time.
-    ///
-    /// # Arguments
-    ///
-    /// * `data` - The data to be serialized, it must be a struct that implements Serialize.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use deboa::{Deboa, errors::DeboaError};
-    /// use deboa_extras::http::serde::json::JsonBody;
-    /// use serde::Serialize;
-    ///
-    /// #[derive(Serialize)]
-    /// struct Post {
-    ///     id: u32,
-    ///     title: String,
-    ///     body: String,
-    /// }
-    ///
-    /// #[tokio::main]
-    /// async fn main() -> Result<(), DeboaError> {
-    ///   let mut api = Deboa::new("https://jsonplaceholder.typicode.com")?;
-    ///   api.set_json(Post { id: 1, title: "title".to_string(), body: "body".to_string() })?.post("/posts").await?;
-    ///   Ok(())
-    /// }
-    /// ```
-    ///
     fn serialize<T: Serialize>(&self, data: T) -> Result<Vec<u8>, DeboaError> {
         let result = serde_json::to_vec(&data);
         if let Err(error) = result {
