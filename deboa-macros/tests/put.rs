679596d663c6ce643b4072ee0dcb6df72cf12db4
Araujo, Rogerio <ROGERIO_ARAUJO+thdgit@homedepot.com>
2025-09-02 18:30:47 -0300

    fix: added support to response bodies on bora
    Former-commit-id: 8f76441c6223e59f5efdad4072acb660ccfa1e0a
    Former-commit-id: 2652a7379ffb7ee7aeefc4313e466e72f4ff5345

---
+++ deboa-macros/tests/put.rs
@@ -0,0 +1,37 @@
+use deboa::{Deboa, errors::DeboaError};
+use deboa_macros::bora;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Post {
+    pub title: String,
+    pub body: String,
+    #[serde(rename = "userId")]
+    pub user_id: u32,
+}
+
+#[bora(
+      api(
+        put(name="update_post", path="/posts/<id:i32>", req_body=Post, format="json"),
+      )
+    )]
+pub struct PostService;
+
+#[tokio::test]
+async fn test_put_by_id() -> Result<(), DeboaError> {
+    let deboa = Deboa::new("https://jsonplaceholder.typicode.com")?;
+
+    let mut post_service = PostService::new(deboa);
+
+    post_service
+        .update_post(
+            1,
+            Post {
+                title: "title".to_string(),
+                body: "body".to_string(),
+                user_id: 1,
+            },
+        )
+        .await?;
+    Ok(())
+}
