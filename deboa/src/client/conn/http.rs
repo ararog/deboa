8db30bfb3a1b5fc5caa02a8e3ae72f90c55be7d5
Rogério Araújo <rogerio.araujo@gmail.com>
2025-09-06 20:04:38 -0300

    fix: use hostname as key for connection pools

--- deboa/src/client/conn/http.rs
+++ deboa/src/client/conn/http.rs
@@ -1,4 +1,5 @@
 use bytes::Bytes;
+use http::StatusCode;
 use http_body_util::Full;
 use hyper::{body::Incoming, Request, Response};
 use url::Url;
@@ -15,7 +16,61 @@ pub type Http2Request = hyper::client::conn::http2::SendRequest<Full<Bytes>>;
 
 #[async_trait::async_trait]
 pub trait DeboaHttpConnection<T> {
+    /// Allow create a new connection.
+    ///
+    /// # Arguments
+    ///
+    /// * `url` - The url to connect.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<BaseHttpConnection<T>, DeboaError>` - The connection or error.
+    ///
     async fn connect(url: Url) -> Result<BaseHttpConnection<T>, DeboaError>;
+
+    /// Allow get connection url.
+    ///
+    /// # Returns
+    ///
+    /// * `&Url` - The connection url.
+    ///
     fn url(&self) -> &Url;
+
+    /// Allow send a request.
+    ///
+    /// # Arguments
+    ///
+    /// * `request` - The request to send.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<Response<Incoming>, DeboaError>` - The response or error.
+    ///
     async fn send_request(&mut self, request: Request<Full<Bytes>>) -> Result<Response<Incoming>, DeboaError>;
+
+    fn process_response(
+        &mut self,
+        url: Url,
+        method: &str,
+        response: Result<Response<Incoming>, hyper::Error>,
+    ) -> Result<Response<Incoming>, DeboaError> {
+        if let Err(err) = response {
+            return Err(DeboaError::Request {
+                host: url.host().unwrap().to_string(),
+                path: url.path().to_string(),
+                method: method.to_string(),
+                message: err.to_string(),
+            });
+        }
+
+        let response = response.unwrap();
+        if !response.status().is_success() || response.status() == StatusCode::TOO_MANY_REQUESTS {
+            return Err(DeboaError::Response {
+                status_code: response.status(),
+                message: response.status().to_string(),
+            });
+        }
+
+        Ok(response)
+    }
 }
