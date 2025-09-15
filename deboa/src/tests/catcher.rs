2ff30f54a1755bc1bb48a2f10f1c59a3f27faef1
Rogério Araújo <rogerio.araujo@gmail.com>
2025-09-15 09:41:54 -0300

    tests(deboa): use httpmock for catcher unit tests

--- deboa/src/tests/catcher.rs
+++ deboa/src/tests/catcher.rs
@@ -1,10 +1,12 @@
 use http::{HeaderMap, HeaderName, HeaderValue, StatusCode};
+use httpmock::MockServer;
 
 use crate::{
     Deboa,
     catcher::{DeboaCatcher, MockDeboaCatcher},
     request::DeboaRequest,
     response::DeboaResponse,
+    tests::utils::setup_server,
 };
 
 #[tokio::test]
@@ -26,32 +28,46 @@ async fn test_catcher_request() {
 
 #[tokio::test]
 async fn test_catcher_response() {
-    let mut mock = MockDeboaCatcher::new();
-    mock.expect_on_request().times(1).returning(move |_| Ok(None));
-    mock.expect_on_response().times(1).returning(move |res| {
+    let server = MockServer::start();
+
+    let http_mock = setup_server(&server, "/get", StatusCode::OK).unwrap();
+
+    let mut catcher_mock = MockDeboaCatcher::new();
+    catcher_mock.expect_on_request().times(1).returning(move |_| Ok(None));
+    catcher_mock.expect_on_response().times(1).returning(move |res| {
         res.set_raw_body(b"test");
     });
 
-    let client = Deboa::builder().catch(mock).build();
-    let response = DeboaRequest::get("https://httpbin.org/get").go(client).await.unwrap();
+    let client = Deboa::builder().catch(catcher_mock).build();
+    let response = DeboaRequest::get(server.url("/get").as_str()).go(client).await.unwrap();
+
+    http_mock.assert();
+
     assert_eq!(response.raw_body(), b"test");
 }
 
 #[tokio::test]
 async fn test_catcher_early_response() {
-    let mut mock = MockDeboaCatcher::new();
+    let server = MockServer::start();
+
+    let http_mock = setup_server(&server, "/get", StatusCode::OK).unwrap();
+
+    let mut catcher_mock = MockDeboaCatcher::new();
 
     let mut headers = HeaderMap::new();
     headers.insert(HeaderName::from_static("test"), HeaderValue::from_static("test"));
 
-    mock.expect_on_request()
+    catcher_mock
+        .expect_on_request()
         .times(1)
         .returning(move |_| Ok(Some(DeboaResponse::new(StatusCode::OK, headers.clone(), b"test"))));
 
-    mock.expect_on_response().times(1).return_const(());
+    catcher_mock.expect_on_response().times(1).return_const(());
 
-    let client = Deboa::builder().catch(mock).build();
-    let response = DeboaRequest::get("https://httpbin.org/get").go(client).await.unwrap();
+    let client = Deboa::builder().catch(catcher_mock).build();
+    let response = DeboaRequest::get(server.url("/get").as_str()).go(client).await.unwrap();
+
+    http_mock.assert_calls(0);
 
     assert_eq!(response.headers().get("test").unwrap(), "test");
 }
