7cd6039686aafe48f020c6db728b86a8a5e6c23d
Rogerio Araujo <rogerio.araujo@gmail.com>
2025-09-29 14:12:26 -0300

    style(deboa-macros): replaced arrows by fat arrows on macros

--- deboa-macros/tests/delete.rs
+++ deboa-macros/tests/delete.rs
@@ -4,7 +4,7 @@ use deboa_macros::delete;
 #[tokio::test]
 async fn delete() -> Result<(), DeboaError> {
     let mut client = deboa::Deboa::new();
-    let response = delete!("https://jsonplaceholder.typicode.com/posts/1" -> &mut client);
+    let response = delete!("https://jsonplaceholder.typicode.com/posts/1" => &mut client);
     assert!(response.status().is_success());
     Ok(())
 }
