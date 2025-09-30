9fc4a695471d6310dfa1f67ce182660426f000b9
Rogerio Araujo <rogerio.araujo@gmail.com>
2025-09-30 09:20:15 -0300

    docs(vamo-macros): fixed documentation, added README.

--- vamo-macros/tests/derive.rs
+++ vamo-macros/tests/derive.rs
@@ -1,26 +1,41 @@
-use deboa::client::serde::RequestBody;
+use deboa::{client::serde::RequestBody, errors::DeboaError};
 use deboa_extras::http::serde::json::JsonBody;
-use vamo::resource::Resource;
+use deboa_tests::utils::setup_server;
+use http::StatusCode;
+use httpmock::{Method::POST, MockServer};
+use serde::Serialize;
+use vamo::{resource::AsPostRequest, Vamo};
 
-#[derive(vamo_macros::Resource)]
+#[derive(vamo_macros::Resource, Serialize)]
 #[post("/users")]
-#[put("/users/{id}")]
-#[patch("/users/{id}")]
-#[delete("/users/{id}")]
+#[put("/users/{}")]
+#[patch("/users/{}")]
+#[delete("/users/{}")]
 #[body_type(JsonBody)]
 pub struct User {
-    id: String,
+    #[rid]
+    id: i32,
     name: String,
 }
 
 #[tokio::test]
-async fn test_resource() -> () {
+async fn test_post_resource() -> Result<(), DeboaError> {
+    let server = MockServer::start();
+    let mock = setup_server(&server, "/api/users", POST, StatusCode::CREATED);
+
     let user = User {
-        id: "1".to_string(),
-        name: "John Doe".to_string(),
+        id: 32,
+        name: "User 1".to_string(),
     };
-    assert_eq!(user.post_path(), "/users");
-    assert_eq!(user.put_path(), "/users/{id}");
-    assert_eq!(user.patch_path(), "/users/{id}");
-    assert_eq!(user.delete_path(), "/users/{id}");
+
+    let mut url = server.base_url();
+    url.push_str("/api");
+    let mut vamo = Vamo::new(url.to_string())?;
+    let response = vamo.go(user.as_post_request()?).await?;
+
+    mock.assert();
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    Ok(())
 }
