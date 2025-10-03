2f1f42ef9f145a5b5480e4c3d946d26dc96bbc30
Rogério Araújo <rogerio.araujo@gmail.com>
2025-10-03 20:40:11 -0300

    fix(deboa): better handling of x-www-urlencoded forms. Also added unit and integrated tests.

--- deboa/src/tests/integrated/post.rs
+++ deboa/src/tests/integrated/post.rs
@@ -1,6 +1,6 @@
-use crate::errors::DeboaError;
 use crate::request::DeboaRequest;
 use crate::Deboa;
+use crate::{errors::DeboaError, form::Form};
 use http::{header, StatusCode};
 use httpmock::{Method::POST, MockServer};
 
@@ -48,4 +48,49 @@ async fn test_post() -> Result<(), DeboaError> {
 #[apply(test!)]
 async fn test_post() -> Result<(), DeboaError> {
     do_post().await
+}
+
+async fn do_post_form() -> Result<(), DeboaError> {
+    let server = MockServer::start();
+
+    let http_mock = server.mock(|when, then| {
+        when.method(POST)
+            .path("/posts")
+            .header(header::CONTENT_TYPE.as_str(), mime::WWW_FORM_URLENCODED.to_string())
+            .body("name=deboa&version=0.0.1");
+        then.status::<u16>(StatusCode::CREATED.into())
+            .header(header::CONTENT_TYPE.as_str(), mime::TEXT_PLAIN.to_string())
+            .body("ping");
+    });
+
+    let mut client = Deboa::new();
+
+    let form = Form::builder().field("name", "deboa").field("version", "0.0.1").build();
+
+    let request = DeboaRequest::post(server.url("/posts").as_str())?
+        .header(header::CONTENT_TYPE, mime::WWW_FORM_URLENCODED.into())
+        .text(&form)
+        .build()?;
+
+    let response = client.execute(request).await?;
+
+    http_mock.assert();
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+    assert_eq!(response.raw_body(), b"ping");
+
+    Ok(())
+}
+
+#[cfg(feature = "tokio-rt")]
+#[tokio::test]
+async fn test_post_form() -> Result<(), DeboaError> {
+    do_post_form().await?;
+    Ok(())
+}
+
+#[cfg(feature = "smol-rt")]
+#[apply(test!)]
+async fn test_post_form() -> Result<(), DeboaError> {
+    do_post_form().await
 }
