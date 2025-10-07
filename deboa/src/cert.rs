bdced43fba58705fba09f00e1d03ea9bbfe157fe
Rogério Araújo <rogerio.araujo@gmail.com>
2025-10-06 21:47:41 -0300

    feat(deboa): added support to provide custom ca cert.

--- deboa/src/cert.rs
+++ deboa/src/cert.rs
@@ -1,11 +1,12 @@
 pub struct ClientCert {
     cert: String,
     pw: String,
+    ca: Option<String>,
 }
 
 impl ClientCert {
-    pub fn new(cert: String, pw: String) -> Self {
-        ClientCert { cert, pw }
+    pub fn new(cert: String, pw: String, ca: Option<String>) -> Self {
+        ClientCert { cert, pw, ca }
     }
 
     pub fn cert(&self) -> &str {
@@ -14,5 +15,9 @@ impl ClientCert {
 
     pub fn pw(&self) -> &str {
         &self.pw
+    }
+
+    pub fn ca(&self) -> Option<&str> {
+        self.ca.as_deref()
     }
 }
--- deboa/src/rt/tokio/http2.rs
+++ deboa/src/rt/tokio/http2.rs
@@ -5,6 +5,7 @@ use hyper::{body::Incoming, client::conn::http2::handshake, Request, Response};
 use hyper_util::rt::TokioExecutor;
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpStream;
+use tokio_native_tls::native_tls::Certificate;
 use tokio_native_tls::native_tls::Identity;
 use tokio_native_tls::native_tls::TlsConnector;
 use url::{Host, Url};
@@ -60,7 +61,8 @@ impl DeboaHttpConnection for BaseHttpConnection<Http2Request> {
                     }
 
                     let socket = stream.unwrap();
-                    let connector = if let Some(client_cert) = client_cert {
+                    let mut builder = TlsConnector::builder();
+                    if let Some(client_cert) = client_cert {
                         let file = std::fs::read(client_cert.cert());
                         if let Err(e) = file {
                             return Err(DeboaError::ClientCert { message: e.to_string() });
@@ -69,10 +71,19 @@ impl DeboaHttpConnection for BaseHttpConnection<Http2Request> {
                         if let Err(e) = identity {
                             return Err(DeboaError::ClientCert { message: e.to_string() });
                         }
-                        TlsConnector::builder().identity(identity.unwrap()).build().unwrap()
-                    } else {
-                        TlsConnector::builder().build().unwrap()
-                    };
+                        builder.identity(identity.unwrap());
+
+                        if let Some(ca) = client_cert.ca() {
+                            let pem = std::fs::read(ca);
+                            if let Err(e) = pem {
+                                return Err(DeboaError::ClientCert { message: e.to_string() });
+                            }
+                            let cert = Certificate::from_pem(&pem.unwrap());
+                            builder.add_root_certificate(cert.unwrap());
+                        }
+                    }
+
+                    let connector = builder.build().unwrap();
                     let connector = tokio_native_tls::TlsConnector::from(connector);
                     let stream = connector.connect(&host.to_string(), socket).await;
 
