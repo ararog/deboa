f8452c70194de8dbd96b06763e4e828e904db3ad
Rogério Araújo <rogerio.araujo@gmail.com>
2025-10-12 11:20:10 -0300

    fix(deboa): fixed issues on multipart form generation, added unit test

--- deboa/src/tests/form.rs
+++ deboa/src/tests/form.rs
@@ -1,16 +1,32 @@
 use crate::{
-    form::{DeboaForm, EncodedForm},
+    form::{DeboaForm, EncodedForm, MultiPartForm},
     Result,
 };
 
 #[test]
-fn test_form() -> Result<()> {
+fn test_encoded_form() -> Result<()> {
     let form = EncodedForm::builder()
         .field("name", "deboa")
         .field("version", "0.0.1")
         .build();
 
     assert_eq!(form, "name=deboa&version=0.0.1");
+
+    Ok(())
+}
+
+#[test]
+fn test_multipart_form() -> Result<()> {
+    let mut builder = MultiPartForm::builder();
+
+    builder.field("name", "deboa");
+    builder.field("version", "0.0.1");
+
+    let form = builder.build();
+
+    print!("{}", form);
+
+    assert_eq!(form, format!("\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\ndeboa\r\n{}\r\nContent-Disposition: form-data; name=\"version\"\r\n\r\n0.0.1\r\n{}--", builder.boundary(), builder.boundary()));
 
     Ok(())
 }
