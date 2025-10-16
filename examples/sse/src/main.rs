715aa2173d4356f7018716f39fafa7fbdea4cfca
Rogério Araújo <rogerio.araujo@gmail.com>
2025-10-16 10:16:50 -0300

    docs(examples) improved chat and sse examples.

--- examples/sse/src/main.rs
+++ examples/sse/src/main.rs
@@ -1,18 +1,29 @@
-use deboa::{Deboa, errors::DeboaError};
-use deboa_extras::http::sse::response::IntoStream;
+use deboa::{Deboa, Result};
+use deboa_extras::http::sse::response::{EventHandler, IntoStream};
 
 #[tokio::main]
-async fn main() -> Result<(), DeboaError> {
+async fn main() -> Result<()> {
     let mut client = Deboa::new();
 
     let response = client.execute("https://sse.dev/test").await?.into_stream();
 
+    let handler = SSEHandler;
+
     response
-        .poll_event(|event| {
-            println!("event: {}", event);
-            Ok(())
-        })
+        .poll_event(handler)
         .await?;
 
+    println!("Connection closed");
+
     Ok(())
+}
+
+pub struct SSEHandler;
+
+#[deboa::async_trait]
+impl EventHandler for SSEHandler {
+    async fn on_event(&mut self, event: &str) -> Result<()> {
+        println!("event: {}", event);
+        Ok(())
+    }
 }
