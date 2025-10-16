08b1a9c4246a34acf5b72c2ea913e90fed2be3fb
Rogério Araújo <rogerio.araujo@gmail.com>
2025-10-16 10:16:50 -0300

    docs(examples) improved chat and sse examples.

--- examples/chat/src/main.rs
+++ examples/chat/src/main.rs
@@ -1,9 +1,9 @@
-use std::io::stdin;
+use std::{io::stdin, sync::{Arc, RwLock}};
 
 use deboa::{Deboa, Result, request::DeboaRequestBuilder};
 use deboa_extras::http::ws::{
     request::WebsocketRequestBuilder,
-    response::{IntoStream, MessageHandler},
+    response::{IntoStream, Message, MessageHandler},
 };
 
 #[tokio::main]
@@ -16,13 +16,15 @@ async fn main() -> Result<()> {
         .into_stream()
         .await;
 
-    let mut handler = ChatHandler;
-
     let (mut reader, mut writer) = response.split();
+
+    let handler = Arc::new(RwLock::new(ChatHandler));
+
+    let reader_handler = Arc::clone(&handler);
 
     let reader_task = tokio::spawn(async move {
         while let Ok(Some(message)) = reader.read_message().await {
-            handler.on_message(message);
+            reader_handler.write().unwrap().on_message(message);
         }
     });
 
@@ -34,9 +36,21 @@ async fn main() -> Result<()> {
             break;
         }
 
-        let result = writer.send_message(&message).await;
-        if result.is_err() {
+        if message.trim() == "exit" {
+            let result = writer.send_close(1000, "Normal Closure").await;
+            if result.is_err() {
+                return Err(deboa::errors::DeboaError::WebSocket {
+                    message: "Failed to send close connection".to_string(),
+                });
+            }
             break;
+        }
+
+        let result = writer.send_text(&message).await;
+        if result.is_err() {
+            return Err(deboa::errors::DeboaError::WebSocket {
+                message: "Failed to send text message".to_string(),
+            });
         }
     }
 
@@ -50,15 +64,23 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Clone)]
 struct ChatHandler;
 
+#[deboa::async_trait]
 impl MessageHandler for ChatHandler {
     fn on_open(&mut self) {
         println!("Connection opened");
     }
 
-    fn on_message(&mut self, message: String) {
-        println!("Received message: {}", message);
+    fn on_message(&mut self, message: Message) {
+        match message {
+            Message::Text(data) => println!("Received message: {}", data),
+            Message::Binary(data) => println!("Received binary message: {}", data.len()),
+            Message::Close(code, reason) => println!("Connection closed: {} {}", code, reason),
+            Message::Ping(data) => println!("Received ping: {}", data.len()),
+            Message::Pong(data) => println!("Received pong: {}", data.len()),
+        }
     }
 
     fn on_close(&mut self) {
