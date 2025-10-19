9f38098421f65284214115e66ec42f9eae5dc05f
Rogério Araújo <rogerio.araujo@gmail.com>
2025-10-19 10:11:18 -0300

    refactor(deboa-extras): rewrite of websocket to make it split into writer and reader and also use it as one stream.

--- deboa-extras/src/http/ws/response.rs
+++ deboa-extras/src/http/ws/response.rs
@@ -1,15 +1,15 @@
-use crate::http::ws::protocol::WebSocket;
-use deboa::response::DeboaResponse;
+use crate::http::ws::protocol::{DeboaWebSocket, UpgradedIo, WebSocket};
+use deboa::{Result, response::DeboaResponse};
 
 #[deboa::async_trait]
 pub trait IntoStream {
-    async fn into_stream(self) -> WebSocket;
+    async fn into_stream(self) -> Result<WebSocket<UpgradedIo>>;
 }
 
 #[deboa::async_trait]
 impl IntoStream for DeboaResponse {
-    async fn into_stream(self) -> WebSocket {
-        let upgraded = self.upgrade().await.expect("Failed to upgrade connection");
-        WebSocket::new(upgraded)
+    async fn into_stream(self) -> Result<WebSocket<UpgradedIo>> {
+        let upgraded = self.upgrade().await?;
+        Ok(WebSocket::new(upgraded))
     }
 }
