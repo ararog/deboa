e398780e1030ad348f601aad63b631297d510fed
Rogério Araújo <rogerio.araujo@gmail.com>
2025-10-24 08:24:59 -0300

    refactor(deboa-extras): added parse function to ServerEvent to allow it parse bytes to a ServerEvent instance.

--- deboa-extras/src/http/sse/io/stream.rs
+++ deboa-extras/src/http/sse/io/stream.rs
@@ -29,31 +29,19 @@ impl ServerEventStream {
 }
 
 impl Stream for ServerEventStream {
-    type Item = Result<Vec<ServerEvent>>;
+    type Item = Result<ServerEvent>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        fn parse_event(data: &Bytes) -> Result<Vec<ServerEvent>> {
-            let data = String::from_utf8_lossy(data.as_ref());
-            let text_message = data;
-            let lines = text_message.lines();
-            let mut events = Vec::new();
-            for line in lines {
-                if let Some(stripped) = line.strip_prefix("data: ") {
-                    if stripped == "[DONE]" {
-                        break;
-                    }
-
-                    events.push(ServerEvent::new(stripped.to_string()));
-                }
-            }
-
-            Ok(events)
-        }
-
         loop {
             return match ready!(self.as_mut().project().stream.poll_frame(cx)) {
                 Some(Ok(frame)) => match frame.into_data() {
-                    Ok(bytes) => Poll::Ready(Some(parse_event(&bytes))),
+                    Ok(bytes) => {
+                        let event = ServerEvent::parse(&bytes);
+                        match event {
+                            Ok(event) => Poll::Ready(Some(Ok(event))),
+                            Err(err) => Poll::Ready(Some(Err(err))),
+                        }
+                    }
                     Err(_) => continue,
                 },
                 Some(Err(err)) => Poll::Ready(Some(Err(DeboaError::SSE(SSEError::ReceiveEvent {
