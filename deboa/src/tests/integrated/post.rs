bac6e469c0dd690ee0f7104e57a7f8e086d5626d
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-10-27 21:28:46 -0300

    fix(deboa): added multipart form test

--- deboa/src/tests/integrated/post.rs
+++ deboa/src/tests/integrated/post.rs
@@ -1,7 +1,5 @@
 use crate::{
-    form::{DeboaForm, EncodedForm},
-    request::DeboaRequest,
-    Deboa, Result,
+    Deboa, Result, form::{DeboaForm, EncodedForm, MultiPartForm}, request::DeboaRequest
 };
 use http::{header, StatusCode};
 use httpmock::{Method::POST, MockServer};
@@ -54,7 +52,7 @@ async fn test_post() -> Result<()> {
     do_post().await
 }
 
-async fn do_post_form() -> Result<()> {
+async fn do_post_encoded_form() -> Result<()> {
     let server = MockServer::start();
 
     let http_mock = server.mock(|when, then| {
@@ -94,13 +92,63 @@ async fn do_post_form() -> Result<()> {
 
 #[cfg(feature = "tokio-rt")]
 #[tokio::test]
-async fn test_post_form() -> Result<()> {
-    do_post_form().await?;
+async fn test_post_encoded_form() -> Result<()> {
+    do_post_encoded_form().await?;
     Ok(())
 }
 
 #[cfg(feature = "smol-rt")]
 #[apply(test!)]
-async fn test_post_form() -> Result<()> {
-    do_post_form().await
+async fn test_post_encoded_form() -> Result<()> {
+    do_post_encoded_form().await
+}
+
+async fn do_post_multipart_form() -> Result<()> {
+    let server = MockServer::start();
+
+    let http_mock = server.mock(|when, then| {
+        when.method(POST)
+            .path("/posts")
+            .header(
+                header::CONTENT_TYPE.as_str(),
+                mime::MULTIPART_FORM_DATA.to_string(),
+            );
+        then.status::<u16>(StatusCode::CREATED.into())
+            .header(header::CONTENT_TYPE.as_str(), mime::TEXT_PLAIN.to_string())
+            .body("ping");
+    });
+
+    let mut client = Deboa::new();
+
+    let form = MultiPartForm::builder()
+        .field("name", "deboa")
+        .field("version", "0.0.1")
+        .build();
+
+    let request = DeboaRequest::post(server.url("/posts").as_str())?
+        .header(header::CONTENT_TYPE, mime::MULTIPART_FORM_DATA.essence_str())
+        .text(&form)
+        .build()?;
+
+    let mut response = client.execute(request).await?;
+
+    http_mock.assert();
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+    assert_eq!(response.raw_body().await, b"ping");
+
+    Ok(())
+}
+
+#[cfg(feature = "tokio-rt")]
+#[tokio::test]
+async fn test_post_multipart_form() -> Result<()> {
+    do_post_multipart_form().await?;
+    Ok(())
+}
+
+#[cfg(feature = "smol-rt")]
+#[apply(test!)]
+async fn test_post_multipart_form() -> Result<()> {
+    do_post_multipart_form().await
 }
