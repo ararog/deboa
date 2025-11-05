d41fd081aa36f607b833a0dcb10a5ff99be4b13d
Rogério Araújo <rogerio.araujo@gmail.com>
2025-11-05 14:32:13 -0300

    docs(deboa): added doc entries for ClientCert.

--- deboa/src/cert.rs
+++ deboa/src/cert.rs
@@ -5,18 +5,48 @@ pub struct ClientCert {
 }
 
 impl ClientCert {
+    /// Allow create a new ClientCert instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `cert` - The client certificate.
+    /// * `pw` - The client certificate password.
+    /// * `ca` - The client certificate authority.
+    ///
+    /// # Returns
+    ///
+    /// * `ClientCert` - The new ClientCert instance.
+    ///
     pub fn new(cert: String, pw: String, ca: Option<String>) -> Self {
         ClientCert { cert, pw, ca }
     }
 
+    /// Allow get the client certificate.
+    ///
+    /// # Returns
+    ///
+    /// * `&str` - The client certificate.
+    ///
     pub fn cert(&self) -> &str {
         &self.cert
     }
 
+    /// Allow get the client certificate password.
+    ///
+    /// # Returns
+    ///
+    /// * `&str` - The client certificate password.
+    ///
     pub fn pw(&self) -> &str {
         &self.pw
     }
 
+    /// Allow get the client certificate authority.
+    ///
+    /// # Returns
+    ///
+    /// * `Option<&str>` - The client certificate authority.
+    ///
     pub fn ca(&self) -> Option<&str> {
         self.ca.as_deref()
     }
