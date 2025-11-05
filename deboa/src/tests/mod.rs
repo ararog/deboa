eb553e83fa8470f8abed725a85cffed1c9f259ac
Rogério Araújo <rogerio.araujo@gmail.com>
2025-11-05 14:30:10 -0300

    test(deboa): added unit tests for cert and into_url.

--- deboa/src/tests/mod.rs
+++ deboa/src/tests/mod.rs
@@ -1,8 +1,11 @@
 mod cache;
 mod catcher;
+mod cert; 
 mod client;
 mod cookie;
 mod form;
 mod integrated;
 mod request;
 mod response;
+mod url;
+
---
+++ deboa/src/tests/url.rs
@@ -0,0 +1,16 @@
+use crate::url::IntoUrl;
+
+#[test]
+fn test_url() {
+    let url_str = "http://example.com";
+    let url = url_str.parse_url().unwrap();
+    assert_eq!(url.scheme(), "http");
+    assert_eq!(url.host_str(), Some("example.com"));
+}
+
+#[test]
+fn test_url_invalid() {
+    let url_str = "invalid_url";
+    let url = url_str.parse_url();
+    assert!(url.is_err());
+} 
