645e1efbedc58d69505169fb2909183d9e9b107a
Rogério Araújo <rogerio.araujo@gmail.com>
2025-11-05 14:30:10 -0300

    test(deboa): added unit tests for cert and into_url.

---
+++ deboa/src/tests/cert.rs
@@ -0,0 +1,10 @@
+use crate::cert::ClientCert;
+
+#[test]
+fn test_cert_init() {
+    let cert = ClientCert::new("cert".to_string(), "pw".to_string(), None);
+    assert_eq!(cert.cert(), "cert");
+    assert_eq!(cert.pw(), "pw");
+    assert_eq!(cert.ca(), None);
+}
+    
