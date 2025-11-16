e4988a1f0488dc0e134e3f53202c6a35a54e0982
Rogério Araújo <rogerio.araujo@gmail.com>
2025-11-16 18:45:00 -0300

    docs(examples): improved blog example to run requests concurrently.

--- examples/blog/src/main.rs
+++ examples/blog/src/main.rs
@@ -1,6 +1,8 @@
 use deboa::Result;
 use deboa_extras::http::serde::json::JsonBody;
 use serde::Deserialize;
+use tokio::sync::Mutex;
+use std::sync::Arc;
 use vamo::Vamo;
 
 #[derive(Debug, Deserialize)]
@@ -12,22 +14,37 @@ struct Post {
 }
 
 #[tokio::main]
-async fn main() {
-    let posts = fetch_posts().await;
-    if let Ok(posts) = posts {
-        println!("Response: {posts:#?}");
-    } else {
-        println!("Error: {}", posts.unwrap_err());
+async fn main() -> Result<()> {
+    let vamo = Arc::new(Mutex::new(Vamo::new("https://jsonplaceholder.typicode.com")?));
+    let ids = vec![1, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66];
+    let mut handles = vec![];
+    for id in ids {
+        let vamo_clone = Arc::clone(&vamo);
+        handles.push(tokio::spawn(async move {
+            let post = fetch_post(vamo_clone, id).await;
+            if let Ok(post) = post {
+                println!("Response: {post:#?}");
+            } else {
+                println!("Error: {}", post.unwrap_err());
+            }
+        }));
     }
+    
+    for handle in handles {
+        let _ = handle.await;
+    }
+
+    Ok(())
 }
 
-async fn fetch_posts() -> Result<Vec<Post>> {
-    let mut vamo = Vamo::new("https://jsonplaceholder.typicode.com")?;
-    let posts: Vec<Post> = vamo
-        .get("/posts")
+async fn fetch_post(vamo: Arc<Mutex<Vamo>>, id: u32) -> Result<Post> {
+    let post: Post = vamo
+        .lock()
+        .await
+        .get(format!("/posts/{id}").as_str())
         .send()
         .await?
         .body_as(JsonBody)
         .await?;
-    Ok(posts)
+    Ok(post)
 }
