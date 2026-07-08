6b8c242420d73edb8d3fdf38d6fd057ac19a2b5f
Rogério Araújo <rogerio.araujo@gmail.com>
2026-07-08 13:28:01 -0300

    chore: disabling compio while doing build fixes

--- deboa-compio/src/client/http/conn/stream/rustls.rs
+++ deboa-compio/src/client/http/conn/stream/rustls.rs
@@ -10,6 +10,94 @@ use crate::{
     Result,
 };
 
+async fn create_stream(host: &str, port: u16) -> Result<TcpStream> {
+    let resolver = resolver(ResolverConfig::default(), ResolverOpts::default()).await;
+
+    let response = resolver
+        .lookup_ip(host)
+        .await;
+
+    let addr = match response {
+        Ok(response) => response,
+        Err(e) => match e.kind() {
+            ResolveErrorKind::NoRecordsFound { query, .. } => {
+                let query_name = query
+                    .name()
+                    .to_string();
+                return Err(DeboaError::Connection(ConnectionError::Tcp {
+                    host: host.to_string(),
+                    message: format!("Could not resolve host: {}", query_name),
+                }));
+            }
+            _ => {
+                return Err(DeboaError::Connection(ConnectionError::Tcp {
+                    host: host.to_string(),
+                    message: format!("Could not resolve host: {}", e),
+                }));
+            }
+        },
+    };
+
+    let addr = addr
+        .iter()
+        .next()
+        .expect("no addresses returned!");
+
+    let tcp_stream = TcpStream::connect((addr, port)).await;
+    let tcp_stream = match tcp_stream {
+        Ok(tcp_stream) => tcp_stream,
+        Err(e) => {
+            return Err(DeboaError::Connection(ConnectionError::Tcp {
+                host: host.to_string(),
+                message: format!("Could not connect to server: {}", e),
+            }));
+        }
+    };
+
+    Ok(tcp_stream)
+}
+
+pub(crate) async fn plain_connection(host: &str, port: u16) -> Result<CompioStream> {
+    let stream = create_stream(host, port).await?;
+    Ok(CompioStream::Plain(stream))
+}
+
+pub(crate) async fn tls_connection(
+    host: &str,
+    port: u16,
+    identity: &Option<DeboaIdentity>,
+    certificate: &Option<DeboaCertificate>,
+    skip_server_verification: bool,
+    alpn: Vec<Vec<u8>>,
+) -> Result<CompioStream> {
+    let socket = create_stream(host, port).await?;
+    let config = setup_rust_tls(host, identity, certificate, skip_server_verification, alpn)?;
+    let connector = TlsConnector::from(Arc::new(config));
+    let hostname = ServerName::try_from(host.to_string());
+
+    if let Err(e) = hostname {
+        return Err(DeboaError::Connection(ConnectionError::Tls {
+            host: host.to_string(),
+            message: e.to_string(),
+        }));
+    }
+    let hostname = hostname.unwrap();
+
+    let stream = connector
+        .connect(&hostname.to_str(), socket)
+        .await;
+
+    if let Err(e) = stream {
+        return Err(DeboaError::Connection(ConnectionError::Tls {
+            host: host.to_string(),
+            message: format!("Could not connect to server: {}", e),
+        }));
+    }
+
+    let stream = stream.unwrap();
+    Ok(CompioStream::Tls(stream))
+}
+
 pub(crate) fn default_provider() -> Arc<rustls::crypto::CryptoProvider> {
     #[cfg(feature = "__rustls_aws_lc_rs")]
     let provider = rustls::crypto::aws_lc_rs::default_provider();
--- deboa-compio/src/rt/mod.rs
+++ deboa-compio/src/rt/mod.rs
@@ -1,3 +1,2 @@
 pub(crate) mod io;
 pub(crate) mod stream;
-pub(crate) mod tls;
