521cadef6d201f7684b355bf4e62ef7e90fd66f4
Rogério Araújo <rogerio.araujo@gmail.com>
2026-08-19 21:10:49 -0300

    chore: some additional fixes

--- deboa-compio/tests/common/helpers.rs
+++ deboa-compio/tests/common/helpers.rs
@@ -72,8 +72,12 @@ pub async fn tls_mock_server() -> EasyHttpMock<VetisAdapter> {
 
     let vetis_adapter_config = VetisAdapterConfig::builder()
         .hostname(&hostname)
-        .interface(&interface)
-        .protocol_version(protocol_version())
+        .interface(
+            interface
+                .parse()
+                .unwrap(),
+        )
+        .protos(vec![protocol_version()])
         .with_random_port()
         .cert(server_cert.to_vec())
         .key(server_key.to_vec())
@@ -102,8 +106,12 @@ pub async fn plain_mock_server() -> EasyHttpMock<VetisAdapter> {
 
     let vetis_adapter_config = VetisAdapterConfig::builder()
         .hostname(&hostname)
-        .interface(&interface)
-        .protocol_version(protocol_version())
+        .interface(
+            interface
+                .parse()
+                .unwrap(),
+        )
+        .protos(vec![protocol_version()])
         .with_random_port()
         .build();
 
